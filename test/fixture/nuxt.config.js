990647b2c872196b04f60ef5e9d72d5bf6d84a15
Ahad Birang <farnabaz@gmail.com>
2019-03-17 17:18:01 +0330

    chore(pkg): upgrade nuxt-edge^3.0.0

--- test/fixture/nuxt.config.js
+++ test/fixture/nuxt.config.js
@@ -3,7 +3,7 @@ const { resolve } = require('path')
 module.exports = {
   rootDir: resolve(__dirname, '../..'),
   srcDir: __dirname,
-  dev: true,
+  dev: false,
   render: {
     resourceHints: false
   },
