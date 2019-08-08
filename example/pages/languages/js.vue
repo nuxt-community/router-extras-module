b5e3a20362db5e197cbd7fe905c0100c7a1dc833
Ahad Birang <farnabaz@gmail.com>
2019-08-08 17:33:48 +0430

    add complete js example

---
+++ example/pages/languages/js.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    JavaScript Router Block {{ routerProp }}
+  </div>
+</template>
+
+<router lang="js">
+{
+  name: 'js-example',
+  path: '/lang/js',
+  caseSensitive: true,
+  alias: [
+    {
+      path: '/lang/javascript',
+      redirect: '/lang/js',
+    }
+  ],
+  props: function(route) {
+    return {
+      routerProp: 9
+    }
+  },
+  beforeEnter: function(to, from, next) {
+    console.log(`from: ${from.path} -> to: ${to.path}`)
+    next();
+  }
+}
+</router>
+
+<script>
+export default {
+  props: {
+    routerProp: {
+      type: Number,
+      default: 0
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
